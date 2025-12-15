d92d8546560eeb11f53897c8b3155a4318934c4d
Thibaut-dbd <random@gmail.com>
2025-12-16 00:24:48 +0100

    J'ai rassemblé ce qu'avait fait ando pour essayer de le debug dans ma branche J'ai pu intrége mes networks  [EN LOCAL] Mdp admin postgreSQL : DEMANDEATITI ( je l'ai mis dans le git secret ) J'ai mis la création de l'alb dans network car trop chiant en cas de pb on peut pas le modifié et ça créer des conflits
    terraform  fmt > ok
    terraform validate > ok
    terraform plan > ok
    terraform apply > ok
    
    J'ai aussi changer le Terraform CI/CD & n8n App Deploy dans ma branche pour qu'il puisse faire aller jusqu'au plan puis il attend une validation avant d 'apply
    ( ça évite de tous casser tmtc)
    
    Le redis met du temps ça mère on dirait une mamie ( entre 15 - 20min en SKU Basic)

--- network.tf
+++ network.tf
@@ -1,3 +1,4 @@
+# network.tf
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet1"
@@ -6,35 +7,31 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
+# Subnet pour AKS (Standard, pas de délégation)
 resource "azurerm_subnet" "Subnet1" {
   name                 = "Snet-Aks"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-  delegation {
-    name = "delegation"
-
-    service_delegation {
-      name    = "Microsoft.ServiceNetworking/trafficControllers"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
-    }
-  }
 }
 
-resource "azurerm_application_load_balancer_subnet_association" "albsubnet" {
-  name = "vnet-link"
-  application_load_balancer_id =  "/subscriptions/cd3fa1ba-5253-4f92-8571-9b1fde759c19/resourceGroups/RG-N8N-AKS/providers/Microsoft.ServiceNetworking/trafficControllers/AGC-N8N-AKS"
-  subnet_id = azurerm_subnet.Subnet1.id
-}
-
-
+# Subnet pour la DB (Délégation pour Postgres Flexible)
 resource "azurerm_subnet" "Subnet2" {
   name                 = "Snet-DB"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "fs"
+    service_delegation {
+      name    = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
 }
 
+# Subnet Admin (Standard)
 resource "azurerm_subnet" "Subnet3" {
   name                 = "Snet-ADMIN"
   resource_group_name  = data.azurerm_resource_group.rg.name
@@ -42,3 +39,32 @@ resource "azurerm_subnet" "Subnet3" {
   address_prefixes     = ["10.0.3.0/24"]
 }
 
+# Subnet dédié ALB (Délégation pour Traffic Controller)
+resource "azurerm_subnet" "Subnet_ALB" {
+  name                 = "Snet-ALB"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.4.0/24"]
+
+  delegation {
+    name = "delegation-alb"
+    service_delegation {
+      name    = "Microsoft.ServiceNetworking/trafficControllers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
+
+# Création de l'ALB (C'est ce qui manquait pour l'erreur 404)
+resource "azurerm_application_load_balancer" "alb" {
+  name                = "AGC-N8N-AKS"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# Association Subnet <-> ALB
+resource "azurerm_application_load_balancer_subnet_association" "albsubnet" {
+  name                         = "vnet-link"
+  application_load_balancer_id = azurerm_application_load_balancer.alb.id
+  subnet_id                    = azurerm_subnet.Subnet_ALB.id
+}
