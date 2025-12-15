290c34733c72e11e7bbbd41e7628fe724588bf68
Thibaut-dbd <random@gmail.com>
2025-12-16 00:28:57 +0100

    J'ai rassemblé ce qu'avait fait ando pour essayer de le debug dans ma branche J'ai pu intrége mes networks  [EN LOCAL] Mdp admin postgreSQL : DEMANDEATITI ( je l'ai mis dans le git secret ) J'ai mis la création de l'alb dans network car trop chiant en cas de pb on peut pas le modifié et ça créer des conflits
    terraform  fmt > ok
    terraform validate > ok
    terraform plan > ok
    terraform apply > ok
    
    J'ai aussi changer le Terraform CI/CD & n8n App Deploy dans ma branche pour qu'il puisse faire aller jusqu'au plan puis il attend une validation avant d 'apply
    ( ça évite de tous casser tmtc)
    
    Le redis met du temps ça mère on dirait une mamie ( entre 15 - 20min en SKU Basic)

---
+++ main.tf
@@ -0,0 +1,173 @@
+# main.tf
+
+data "azurerm_client_config" "current" {}
+
+/*
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+
+
+
+data "azurerm_virtual_network" "vnet" {
+  name                = "vnet1"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+
+data "azurerm_subnet" "snet_aks" {
+  name                 = "Snet-Aks"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+}
+
+data "azurerm_subnet" "snet_admin_agic" {
+  name                 = "Snet-ADMIN"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+}
+
+data "azurerm_subnet" "snet_db" {
+  name                 = "Snet-DB"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+}
+*/
+
+resource "azurerm_container_registry" "acr" {
+  name                = "${var.acr_name_prefix}${random_integer.suffix.result}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "random_integer" "suffix" {
+  min = 1000
+  max = 9999
+}
+
+resource "azurerm_key_vault" "akv" {
+  name                       = "akv-n8n-tf-secrets"
+  location                   = data.azurerm_resource_group.rg.location
+  resource_group_name        = data.azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+
+}
+# On crée la policy à part
+resource "azurerm_key_vault_access_policy" "terraform_user" {
+  key_vault_id = azurerm_key_vault.akv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Recover", # Ajout utile
+    "Purge"    # Ajout utile
+  ]
+}
+
+resource "azurerm_key_vault_secret" "pg_password" {
+  name         = "pg-admin-password"
+  key_vault_id = azurerm_key_vault.akv.id
+  value        = var.pg_admin_password
+
+  # Cette dépendance force Terraform à attendre que les droits soient appliqués
+  depends_on = [azurerm_key_vault_access_policy.terraform_user]
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-n8n-cluster"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  dns_prefix          = "n8n-aks"
+
+  network_profile {
+    network_plugin = "azure"
+    service_cidr   = "10.10.0.0/16"
+    dns_service_ip = "10.10.0.10"
+  }
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    #vm_size        = "Standard_DS2_v2"
+    vm_size        = "Standard_D2_v3"          # <-- REMPLACER PAR CECI (ou Standard_B2ms)
+    vnet_subnet_id = azurerm_subnet.Subnet1.id # Utilise data # a verif Subnet1 = Snet-Aks non data normal ?
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control_enabled = true
+}
+
+resource "azurerm_private_dns_zone" "pg_private_dns" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "pg_dns_link" {
+  name                  = "link-to-vnet1"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.pg_private_dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_postgresql_flexible_server" "pg" {
+  # name                   = "pg-n8n-tf-server"      <-- ANCIEN NOM bloqué
+  name                   = "pg-n8n-tf-server-v2"   # <-- NOUVEAU NOM
+  resource_group_name    = data.azurerm_resource_group.rg.name
+  location               = data.azurerm_resource_group.rg.location
+  # Astuce : Forcez la zone 1 pour éviter qu'Azure ne choisisse au hasard
+  zone                   = "1"
+  version                = "14"
+  administrator_login    = "n8nadmin"
+  administrator_password = azurerm_key_vault_secret.pg_password.value
+  storage_mb             = 32768
+  sku_name               = "B_Standard_B2s"
+
+  delegated_subnet_id           = azurerm_subnet.Subnet2.id # Utilise data # a verif bizarre le delegette alors que subnet1 service_delegation
+  public_network_access_enabled = false
+  private_dns_zone_id           = azurerm_private_dns_zone.pg_private_dns.id
+}
+
+resource "azurerm_postgresql_flexible_server_database" "n8n_db" {
+  name      = "n8n_db"
+  server_id = azurerm_postgresql_flexible_server.pg.id
+  collation = "en_US.utf8"
+  charset   = "UTF8"
+}
+
+resource "azurerm_redis_cache" "redis" {
+  # name                = "redis-n8n-tf-cache"      <-- L'ancien nom bloqué le 15-12-25 : 23:11 CET
+  name                = "redis-n8n-tf-cache-v2"   # <-- Nouveau nom
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  capacity            = 1
+  family              = "C"
+  sku_name            = "Basic"
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+output "kube_config" {
+  sensitive = true
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+}
+output "redis_primary_key" {
+  sensitive = true
+  value     = azurerm_redis_cache.redis.primary_access_key
+}
---
+++ providers.tf
@@ -0,0 +1,23 @@
+# providers.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  subscription_id = "cd3fa1ba-5253-4f92-8571-9b1fde759c19"
+  features {}
+}
