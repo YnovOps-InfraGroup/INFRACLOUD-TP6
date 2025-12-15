157012db2b912553922f0e41f277e1a095429f4a
Thibaut-dbd <random@gmail.com>
2025-12-15 14:32:03 +0100

    Module update network ok

--- network.tf
+++ network.tf
@@ -11,7 +11,22 @@ resource "azurerm_subnet" "Subnet1" {
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ServiceNetworking/trafficControllers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
 }
+
+resource "azurerm_application_load_balancer_subnet_association" "albsubnet" {
+  name = "albsubnet"
+  application_load_balancer_id =  "/subscriptions/cd3fa1ba-5253-4f92-8571-9b1fde759c19/resourceGroups/RG-N8N-AKS/providers/Microsoft.ServiceNetworking/trafficControllers/AGC-N8N-AKS"
+  subnet_id = azurerm_subnet.Subnet1.id
+}
+
 
 resource "azurerm_subnet" "Subnet2" {
   name                 = "Snet-DB"
@@ -26,3 +41,4 @@ resource "azurerm_subnet" "Subnet3" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.3.0/24"]
 }
+
