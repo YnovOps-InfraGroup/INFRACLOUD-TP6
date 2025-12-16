a1df73a7c668035d81aa7fc2b5ed167734fe96b3
Greg <gregory.myscile@ynov.com>
2025-12-16 13:51:27 +0100

    rbac.tf t fmt ok

--- terraform/rbac.tf
+++ terraform/rbac.tf
@@ -4,10 +4,10 @@
 # Si vous avez une erreur d'authentification, contactez votre administrateur Azure
 # ou utilisez une alternative avec les credentials d'admin ACR dans un imagePullSecret
 resource "azurerm_role_assignment" "aks_acr_pull" {
-  count              = 1  # Set to 0 if you don't have permissions
-  scope              = azurerm_container_registry.acr.id
+  count                = 1 # Set to 0 if you don't have permissions
+  scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id       = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 
   depends_on = [azurerm_kubernetes_cluster.aks, azurerm_container_registry.acr]
 }
