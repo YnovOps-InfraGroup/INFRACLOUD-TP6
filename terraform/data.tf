7b483304f706cefaa78368c44f94776007a599ad
Your Name <you@example.com>
2025-12-16 10:33:18 +0100

    clean ultime

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,13 +3,3 @@ data "azurerm_resource_group" "rg" {
 }
 
 data "azurerm_client_config" "current" {}
-
-variable "rg" {
-  type    = string
-  default = "RG-N8N-AKS"
-}
-
-data "azurerm_resource_group" "rg" {
-  name = var.rg
-
-}
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,4 +1,3 @@
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet1"
   address_space       = ["10.0.0.0/16"]
@@ -11,27 +10,14 @@ resource "azurerm_subnet" "Subnet1" {
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "delegation"
-
-    service_delegation {
-      name    = "Microsoft.ServiceNetworking/trafficControllers"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
-    }
-  }
 }
 
-resource "azurerm_application_load_balancer_subnet_association" "albsubnet" {
-  name                         = "vnet-link"
-  application_load_balancer_id = "/subscriptions/cd3fa1ba-5253-4f92-8571-9b1fde759c19/resourceGroups/RG-N8N-AKS/providers/Microsoft.ServiceNetworking/trafficControllers/AGC-N8N-AKS"
-  subnet_id                    = azurerm_subnet.Subnet1.id
-}
 resource "azurerm_subnet" "Subnet2" {
   name                 = "Snet-DB"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
+
   delegation {
     name = "fs"
     service_delegation {
