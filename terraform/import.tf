72e13414fab7c84461f13096637d18ca3d3502c1
Thibaut-dbd <random@gmail.com>
2025-12-16 11:59:13 +0100

    Output update

--- terraform/import.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = azurerm_redis_cache.redis
-  id = "/subscriptions/cd3fa1ba-5253-4f92-8571-9b1fde759c19/resourceGroups/RG-N8N-AKS/providers/Microsoft.Cache/redis/redis-n8n-tf-cache"
-}
