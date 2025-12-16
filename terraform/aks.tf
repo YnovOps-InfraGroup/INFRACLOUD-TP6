35258de6166bb770cadb85ac4d63797dd81b0d10
Your Name <you@example.com>
2025-12-16 11:36:59 +0100

    gagaougou

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -13,7 +13,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name           = "default"
     node_count     = 3
-    vm_size        = "Standard_DS2_v2"
+    vm_size        = "Standard_D2_v3"
     vnet_subnet_id = azurerm_subnet.Subnet1.id
 
     temporary_name_for_rotation = "tempnode"
